fb01da27424ffcde58e57860d506337fd9e830f1
thraasymaachus <unicycles431@gmail.com>
2023-03-16 19:46:55 +1300

    Add some code to play around with in tinkercad. Code is functional, but assoc tinkercad function is not

--- assignment_1.c
+++ assignment_1.c
@@ -1,5 +1,50 @@
-#include <stdio.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+volatile uint8_t state;
 
-void main() {
-    printf("Hello world\n");
+int main(void){
+// Set in/out
+DDRB |= (1<<DDB1)|(1<<DDB2)|(1<<DDB3); // Set pins 9, 10, 11 to write
+DDRD &= ~(1<<DDD2); // Set pin 3 to read
+
+
+state = 1; // Default on
+
+EICRA |= (1 << ISC00) | (1 << ISC01);  // INT0 will trigger on a rising edge
+EIMSK |= (1 << INT0);                  // INT0 enabled
+
+sei(); // All interupts enabled
+
+// Defaults
+
+PORTB |= (1<<DDB1)|(1<<DDB2)|(1<<DDB3); // Default all LEDs to be on
+
+while(1){
+    if(state == 1){
+        // Blink the LEDs if the state is 1
+        PORTB |= (1<<PB3);
+        PORTB &= ~(1<<PB1);
+        _delay_ms(500);
+        PORTB &= ~(1<<PB3);
+        PORTB |= (1<<PB1);
+        _delay_ms(500);
+    } else {
+        for (int i = 0; i < 4; i++) {
+        
+        PORTB |= (1<<PB3);
+        PORTB &= ~(1<<PB1);
+        _delay_ms(125);
+        PORTB &= ~(1<<PB3);
+        PORTB |= (1<<PB1);
+        _delay_ms(125);
+        
+        }
+    }
+}
+}
+
+
+ISR(INT0_vect){
+state = ~state;
+Serial.print("state switch");
 }
