29b6dd2dab07395851c25bc9eb085e249ac80896
thraasymaachus <unicycles431@gmail.com>
2023-03-19 17:02:07 +1300

    Include ADMUX and ADCSRA parameters in setup

--- assignment_1.c
+++ assignment_1.c
@@ -1,6 +1,5 @@
 #include <util/delay.h>
 #include <avr/interrupt.h>
-volatile uint8_t state;
 
 /*
 Your program should loop, reading the voltage at pin A0 at a rate of 1 Sa/s. Use an external
@@ -26,9 +25,19 @@ int main(void){
 
     threshold = 2; // Voltage threshold to 
 
+    // Note: We don't need to configure MUX3..0 as this defaults to 0000, i.e ADC0
+    ADMUX |= (1 << REFS0);   //  Reference voltage becomes our internal voltage of 1.1V, rather than whatever analog reference is being fed in.
+    ADMUX |= (1 << MUX0);    // 
+    ADMUX |= (1 << ADLAR);   // We'll read in 8 bits of ADC, rather than 10 bits.
+
+    // Note: We can execute this as a single instruction, to reduce the number of clock cycles. Same for the above.
+    ADCSRA |= (1 << ADEN);    // Start the ADC
+    ADCSRA |= (1 << ADPS1);   //with next line…
+    ADCSRA |= (1 << ADPS0);   //set division factor-8 for 125kHz ADC clock
+
     while(1){
         // If pin A0 is greater than threshold, blink LED twice in a second. Otherwise, blink LED once in a second.
-        if ((PINC && (1 << DDC0)) > threshold){
+        if ((PINC && (1 << DDC0)) > threshold){  // ADC = (Vin * 2^10)/(Vref)
             for (int i = 0; i < 2; i++){
                 digitalWrite(LED, HIGH);
                 _delay_ms(100);
