740b86dfa6fcc7e995758538d5b825677e7526fa
Yiyang Chen <109450344+YachtChen@users.noreply.github.com>
2023-04-20 14:47:38 +1200

    Finalised Version for task 2

--- task_2.2.ino
+++ task_2.2.ino
@@ -1,22 +1,25 @@
+//This function store the ADC result into ring buffer Requirement: 
+//Read A0 value
+//Read at a rate of 200 samples/s (5ms to overflow)
+//Use external reference of 5V at AREF
+//Left adjust ADC
+//Use only 8-bits ADC
+//Use Prescalar factor to 128
+//Store value to ring buffer of unsigned integers to be at least 1000 samples. 
+
+
 #include <util/delay.h>
 #include <avr/io.h>   //allows for register commands to be understood
-
-//Store at least 1000 result in a ring buffer (unsigned integers arrays): Done
-//sample rate 200sample/s DONE
-//External 5V at AREF: DONE 
-//Left Adjust ADC: DONE
-//Use only 8 bit resolution: DONE
-//Use ADC prescalar factor to 128: DONE
 
 //Declare variable for ring buffer
 #define SIZE_OF_BUFFER 1000
 int start = -1;
 int end   = -1;
 uint8_t database[SIZE_OF_BUFFER] = {0};
-
  
-//Set state to start ADC
+//Set ADC variables
 bool state = 0;
+uint8_t value;
 
 // Check if the array is full
 bool isFull() {
@@ -26,7 +29,7 @@ bool isFull() {
   return 0;
 }
 
-//Add value to array
+//Add value to end of array
 void add(uint16_t value){
     if (start == -1){
       start = 0;
@@ -35,7 +38,7 @@ void add(uint16_t value){
     database[end] = value;
 }
 
-//Remove value from array
+//Remove first value from array
 void removeFirst(){
   if (start == end) {
       start = -1;
@@ -78,13 +81,13 @@ int main(void) {
   while(1) {
     if(state){
       ADCSRA |= (1 << ADSC);//start conversion
-
+      value = ADCH;
       //Use Ring Buffer to store data
       if(isFull()){
         removeFirst();
-        add(ADCH);
+        add(value);
       }else{
-        add(ADCH);
+        add(value);
       }
 
       // print out the value you read:
